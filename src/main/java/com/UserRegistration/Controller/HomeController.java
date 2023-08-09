c9210c0f83d5f3ec8d917260a6bd21df6683910b
ti <thienphuc484@gmail.com>
2023-08-09 09:22:19 +0700

    Update Home UI

--- src/main/java/com/UserRegistration/Controller/HomeController.java
+++ src/main/java/com/UserRegistration/Controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.UserRegistration.Controller;
 
+import com.UserRegistration.Service.CustomUserDetails;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String homePage(){
+    public String index(Model model, Authentication authentication) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            String firstName = ((CustomUserDetails) authentication.getPrincipal()).getFullName();
+            model.addAttribute("firstName", firstName);
+        }
         return "index";
     }
 }
